911afd09918c7a0da3ee705fe75eaa8aec02cac0
thulasikan <thulasikanswe@gmail.com>
2024-06-24 12:10:07 +0530

    started typescript

--- Extra/Weather25/js/script.js
+++ Extra/Weather25/js/script.js
@@ -1,8 +1,9 @@
 const refresh_btn = document.getElementById("refresh");
 const API_KYEY = "bdebfbd56aa02bc66c48751fd367cc66";
-const cards = document.getElementById('cards');
+const cards = document.getElementById("cards");
 const card = document.querySelectorAll(".card");
 
+ 
 
 function getWeather(city) {
   const weather_url = `https://api.openweathermap.org/data/2.5/weather?q=${city},srilanka&appid=${API_KYEY}`;
@@ -10,16 +11,20 @@ function getWeather(city) {
   return fetch(weather_url)
     .then((response) => response.json())
     .then((data) => {
-      return [data["main"].temp, data["weather"][0].description];
+        console.log(data)
+      return [data["main"].feels_like, data["weather"][0].description];
+      
     })
     .catch((error) => {
       return error + "error";
     });
+    
 }
 
 function kelvinToCelcius(temp) {
   return Math.floor(temp - 273.15);
 }
+
 function createWeatherCard(data) {
   const card = document.createElement("div");
   const temp = document.createElement("p");
@@ -34,16 +39,19 @@ function createWeatherCard(data) {
 
   return card;
 }
+ 
+ 
+ 
 
 
 card.forEach((card) => {
   let city = card.querySelector("button").value;
   getWeather(city).then((data) => {
+ 
     const weather_card = createWeatherCard(data);
+    
     card.appendChild(weather_card);
   });
 });
 
-function sortWeatherCards(){
-    
-}
+// function sortWeatherCards() {}
